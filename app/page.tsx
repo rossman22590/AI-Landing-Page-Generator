6c942aab858d04f614ba52bae5f5997a06fed351
JustAIGithub <enqueueit@gmail.com>
2023-08-13 11:56:50 +0800

    add friend link

--- app/page.tsx
+++ app/page.tsx
@@ -192,8 +192,13 @@ export default function Chat() {
 			<svg width="32" height="32" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
 			    <path fill="#f43f5e" d="M96 63.38C142.49 27.25 201.55 7.31 260.51 8.81c29.58-.38 59.11 5.37 86.91 15.33c-24.13-4.63-49-6.34-73.38-2.45C231.17 27 191 48.84 162.21 80.87c5.67-1 10.78-3.67 16-5.86c18.14-7.87 37.49-13.26 57.23-14.83c19.74-2.13 39.64-.43 59.28 1.92c-14.42 2.79-29.12 4.57-43 9.59c-34.43 11.07-65.27 33.16-86.3 62.63c-13.8 19.71-23.63 42.86-24.67 67.13c-.35 16.49 5.22 34.81 19.83 44a53.27 53.27 0 0 0 37.52 6.74c15.45-2.46 30.07-8.64 43.6-16.33c11.52-6.82 22.67-14.55 32-24.25c3.79-3.22 2.53-8.45 2.62-12.79c-2.12-.34-4.38-1.11-6.3.3a203 203 0 0 1-35.82 15.37c-20 6.17-42.16 8.46-62.1.78c12.79 1.73 26.06.31 37.74-5.44c20.23-9.72 36.81-25.2 54.44-38.77a526.57 526.57 0 0 1 88.9-55.31c25.71-12 52.94-22.78 81.57-24.12c-15.63 13.72-32.15 26.52-46.78 41.38c-14.51 14-27.46 29.5-40.11 45.18c-3.52 4.6-8.95 6.94-13.58 10.16a150.7 150.7 0 0 0-51.89 60.1c-9.33 19.68-14.5 41.85-11.77 63.65c1.94 13.69 8.71 27.59 20.9 34.91c12.9 8 29.05 8.07 43.48 5.1c32.8-7.45 61.43-28.89 81-55.84c20.44-27.52 30.52-62.2 29.16-96.35c-.52-7.5-1.57-15-1.66-22.49c8 19.48 14.82 39.71 16.65 60.83c2 14.28.75 28.76-1.62 42.9c-1.91 11-5.67 21.51-7.78 32.43a165 165 0 0 0 39.34-81.07a183.64 183.64 0 0 0-14.21-104.64c20.78 32 32.34 69.58 35.71 107.48c.49 12.73.49 25.51 0 38.23A243.21 243.21 0 0 1 482 371.34c-26.12 47.34-68 85.63-117.19 108c-78.29 36.23-174.68 31.32-248-14.68A248.34 248.34 0 0 1 25.36 366A238.34 238.34 0 0 1 0 273.08v-31.34C3.93 172 40.87 105.82 96 63.38m222 80.33a79.13 79.13 0 0 0 16-4.48c5-1.77 9.24-5.94 10.32-11.22c-8.96 4.99-17.98 9.92-26.32 15.7z"/>
 			</svg>
-            <h1 className="font-bold text-xl">AI Landing Page Generator</h1>
+            <h1 className="font-bold text-xl">
+				<a href="https://ailandingpagegenerator.com">AI Landing Page Generator</a>
+			</h1>
           </div>
+		  <div className="flex items-center hidden md:block lg:block text-black">
+		      <a href="#contact" className="text font-semibold mr-1 md:mr-4 lg:mr-4">Contact</a>
+		  </div>
         </div>
       </header>
       {isLoading ? null : (
@@ -258,7 +263,7 @@ export default function Chat() {
               </div>
               <div className="flex-1 text-center">
                 <div className="flex items-center justify-center space-x-2 bg-gray-200 rounded-xl mx-8 py-1 px-2">
-                  <span className="text-black">ailandingpagegenerator.com</span>
+                  <span className="text-black hidden md:block lg:block">ailandingpagegenerator.com</span>
                   {isLoading && <span className="ml-4 animate-spin">
 					<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 					    <path fill="none" stroke="#f43f5e" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 14a1 1 0 1 0 2 0a1 1 0 0 0-2 0Zm6-2a1 1 0 1 0 2 0a1 1 0 0 0-2 0Zm6-2a1 1 0 1 0 2 0a1 1 0 0 0-2 0Z"/>
@@ -321,7 +326,7 @@ export default function Chat() {
                 )}
               </div>
             </div>
-            <div className="h-[96rem]">
+            <div className="h-[96rem] flex-grow">
               <Frame
                 ref={iframeRef}
                 sandbox="allow-same-origin allow-scripts"
@@ -337,6 +342,58 @@ export default function Chat() {
           </div>
         </div>
       )}
+	  <div className="text-black pt-10 mt-[460px] md:mr-[60%]">
+	  		<div id="contact" className="text-black pt-4">
+	  		  <div className="text-2xl">Contact</div>
+	  		  <div className="flex justify-start items-center mb-2 space-y-2 mt-2 flex-wrap">
+	  			<a href="https://github.com/JustAIGithub/AI-Landing-Page-Generator" className="text-gray cursor-pointer mr-4">
+	  				<svg width="26" height="26" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+	  				    <path fill="#000000" d="M12 2.247a10 10 0 0 0-3.162 19.487c.5.088.687-.212.687-.475c0-.237-.012-1.025-.012-1.862c-2.513.462-3.163-.613-3.363-1.175a3.636 3.636 0 0 0-1.025-1.413c-.35-.187-.85-.65-.013-.662a2.001 2.001 0 0 1 1.538 1.025a2.137 2.137 0 0 0 2.912.825a2.104 2.104 0 0 1 .638-1.338c-2.225-.25-4.55-1.112-4.55-4.937a3.892 3.892 0 0 1 1.025-2.688a3.594 3.594 0 0 1 .1-2.65s.837-.262 2.75 1.025a9.427 9.427 0 0 1 5 0c1.912-1.3 2.75-1.025 2.75-1.025a3.593 3.593 0 0 1 .1 2.65a3.869 3.869 0 0 1 1.025 2.688c0 3.837-2.338 4.687-4.563 4.937a2.368 2.368 0 0 1 .675 1.85c0 1.338-.012 2.413-.012 2.75c0 .263.187.575.687.475A10.005 10.005 0 0 0 12 2.247Z"/>
+	  				</svg>
+	  			</a>
+	  			<a href="https://twitter.com/AUDI_GUZZ" className="text-gray cursor-pointer mr-4">
+	  				<svg width="26" height="26" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
+	  				    <path fill="#1d9bf0" d="M114.896 37.888c.078 1.129.078 2.257.078 3.396c0 34.7-26.417 74.72-74.72 74.72v-.02A74.343 74.343 0 0 1 0 104.21c2.075.25 4.16.375 6.25.38a52.732 52.732 0 0 0 32.615-11.263A26.294 26.294 0 0 1 14.331 75.09c3.937.76 7.993.603 11.857-.453c-12.252-2.475-21.066-13.239-21.066-25.74v-.333a26.094 26.094 0 0 0 11.919 3.287C5.5 44.139 1.945 28.788 8.913 16.787a74.535 74.535 0 0 0 54.122 27.435a26.277 26.277 0 0 1 7.598-25.09c10.577-9.943 27.212-9.433 37.154 1.139a52.696 52.696 0 0 0 16.677-6.376A26.359 26.359 0 0 1 112.92 28.42A52.227 52.227 0 0 0 128 24.285a53.35 53.35 0 0 1-13.104 13.603z"/>
+	  				</svg>
+	  			</a>
+	  			<a href="https://aicodeconvert.com" className="text-black cursor-pointer rounded-full mr-4">
+	  				<div 
+	  					className="items-center flex text-sm font-medium justify-center py-2 px-4 border rounded-full">
+	  					<svg width="20" height="20" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+	  					    <path fill="#3b82f6" d="M516 673c0 4.4 3.4 8 7.5 8h185c4.1 0 7.5-3.6 7.5-8v-48c0-4.4-3.4-8-7.5-8h-185c-4.1 0-7.5 3.6-7.5 8v48zm-194.9 6.1l192-161c3.8-3.2 3.8-9.1 0-12.3l-192-160.9A7.95 7.95 0 0 0 308 351v62.7c0 2.4 1 4.6 2.9 6.1L420.7 512l-109.8 92.2a8.1 8.1 0 0 0-2.9 6.1V673c0 6.8 7.9 10.5 13.1 6.1zM880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zm-40 728H184V184h656v656z"/>
+	  					</svg>
+	  					<p className="ml-2">AICodeConvert</p>
+	  				</div>
+	  			</a>
+	  		  </div>
+	  		  <div>
+	  			Mail: enqueueit@gmail.com
+	  		  </div>
+	  		</div>
+	  		<div id="friendlyLink" className="mt-4">
+	  			<div className="text-2xl">Friendly Link</div>
+	  			<div className="flex justify-start items-center mb-2 space-y-2 mt-2 flex-wrap">
+	  				<a href="https://twidropper.com" className="text-black cursor-pointer rounded-full mr-4">
+	  					<div 
+	  						className="items-center flex text-sm font-medium justify-center py-2 px-4 border rounded-full">
+	  						<p className="">Twitter動画保存</p>
+	  					</div>
+	  				</a>
+	  				<a href="https://randomlettergenerator.org" className="text-black cursor-pointer rounded-full mr-4">
+	  					<div 
+	  						className="items-center flex text-sm font-medium justify-center py-2 px-4 border rounded-full">
+	  						<p className="">Random Letter Generator</p>
+	  					</div>
+	  				</a>
+	  				<a href="https://crontab.online" className="text-black cursor-pointer rounded-full mr-4">
+	  					<div 
+	  						className="items-center flex text-sm font-medium justify-center py-2 px-4 border rounded-full">
+	  						<p className="">Crontab Online Generator</p>
+	  					</div>
+	  				</a>
+	  			</div>
+	  		</div>
+	  </div>
     </div>
   );
 }
